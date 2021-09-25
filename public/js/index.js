30e23e47cf8964c6497b12cb2f94c9bae646e67f
imsahiljain <emailofsahiljain@gmail.com>
2021-09-25 14:16:24 +0530

    add filter feature in products page

--- public/js/index.js
+++ public/js/index.js
@@ -2,3 +2,4 @@ const navBar = document.querySelector(".navbar");
         window.onscroll = () => {
             this.scrollY > 20 ? navBar.classList.add("sticky") : navBar.classList.remove("sticky"); 
         }
+
